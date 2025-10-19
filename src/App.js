9728d3e993df81fb2e10ff1fa1ee10ddf21b8880
Avik10 <bnk.avik@gmail.com>
2025-10-19 11:48:09 +0530

    update comp name

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Experaince from "./components/experances/expeiance";
+import Experaince from "./components/expeiance/expeiance";
 import Intro from "./components/Intro/Intro";
 import Navbar from "./components/Navbar/Navbar";
 import Skills from "./components/Skills/Skills";
